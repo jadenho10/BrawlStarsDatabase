d3fff0491e35daba2f0bf4e96c1ebf2215d9caf9
Jaden <jaden.ho@gmail.com>
2024-06-26 12:23:20 -0700

    UI changes

---
+++ brawlstarsdatabase/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import axios from 'axios'
+import Navbar from './Components/Navbar'
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
---
+++ brawlstarsdatabase/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>Brawl Stars Database</div>
+    )
+}
+
+export default Navbar
